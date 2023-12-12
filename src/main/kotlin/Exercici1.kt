6e7e126bb7969935b13c4c2bb9ce7925c74550df
Fabián <fabian.gomez.7e3@itb.cat>
2023-12-12 18:49:13 +0100

    Ejercicio 1 con la instalacion del Dokka y el JUnit pero aun sin implementar la documentacion ni el sistema de Test

---
+++ src/main/kotlin/Exercici1.kt
@@ -0,0 +1,10 @@
+fun main(){
+}
+val grup = arrayOf("Niki", "Omar", "Fabian")
+fun quiEsIntegrantDAquestGrup(num:Int):String {
+    var result = ""
+    if (num < grup.size)
+        result = grup[num]
+
+    return result
+}
