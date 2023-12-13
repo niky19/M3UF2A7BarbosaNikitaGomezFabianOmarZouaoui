99f835a5474758ef6501fb706596fd271ba685f7
niky <nikita.barbosa.7e5@itb.cat>
2023-12-13 21:51:29 +0100

    Canvis i testos

--- src/main/kotlin/Exercici1.kt
+++ src/main/kotlin/Exercici1.kt
@@ -1,18 +1,23 @@
-fun main(){
-}
-
 /**
- * @author Fabián Gómez
+ * @author Fabián Gómez, Nikita Barbosa, Omar Zouaoui
  * @date 13/12/2023
- * Detecta i mostra el numero de cada integrant del grup.
- * @see quiEsIntegrantDAquestGrup
+ * Mostra per pantalla el nom de l'integrant del grup que ocupa la posició indicada per paràmetre
  */
 
-val grup = arrayOf("Niki", "Omar", "Fabian")
 fun quiEsIntegrantDAquestGrup(num:Int):String {
+    val grup = arrayOf("Niki", "Omar", "Fabian")
     var result = ""
-    if (num < grup.size)
-        result = grup[num]
-
+    if (num in 1.. grup.size) {
+        result = grup[num-1]
+    }
+    else result = "\"\""
     return result
+}
+
+fun main() {
+    println("Tenim un equip format per ${quiEsIntegrantDAquestGrup(1)}, ${quiEsIntegrantDAquestGrup(2)} i ${quiEsIntegrantDAquestGrup(3)}")
+    println(quiEsIntegrantDAquestGrup(1))
+    println(quiEsIntegrantDAquestGrup(2))
+    println(quiEsIntegrantDAquestGrup(3))
+    println(quiEsIntegrantDAquestGrup(4))
 }
