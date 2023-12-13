67e85ba7b9cac8a058b15ef3453b74161275e759
niky <nikita.barbosa.7e5@itb.cat>
2023-12-13 22:08:38 +0100

    ex2, resultat fixed

--- src/main/kotlin/Exercici2.kt
+++ src/main/kotlin/Exercici2.kt
@@ -1,20 +1,37 @@
-/*
-Escriu una funció anomenada “calcularIVA” a la que se li passa un preu, un
-tipus d’IVA i la data de la compra i retorna el preu amb l’IVA aplicat. El tipus
-d’IVA pot ser General, Reduït, Superreduït o Exempt. El percentatge a aplicar
-és el vigent en la data de compra i el podem trobar en aquesta taula:
- */
+/**
+ * Calcula el preu amb IVA d'un producte en funció del tipus d'IVA i la data de compra
+ * @param preuSenseIVA Double
+ * @param tipusIVA TipusdeIVA
+ * @param dataCompra LocalDate
+ * @return Double
+ * @author Fabián Gomez, Nikita Barbosa, Omar Zouaoui
+ **/
 
 import java.text.SimpleDateFormat
 import java.util.*
 
-enum class TipusIVA(val dataIniciVigencia: Date, val preuGeneral: Double, val preuReduit: Double, val preuSuperreduit: Double, val preuExempt: Double) {
-    INICIAL(parseDate("01-01-1986"), 0.12, 0.06, 0.0, 0.0),
-    ANY_1992(parseDate("01-01-1992"), 0.15, 0.06, 0.0, 0.0),
-    ANY_1993(parseDate("01-01-1993"), 0.15, 0.06, 0.03, 0.0),
-    ANY_1995(parseDate("01-01-1995"), 0.16, 0.07, 0.04, 0.0),
-    ANY_2010(parseDate("01-01-2010"), 0.18, 0.08, 0.04, 0.0),
-    JULIOL_2012(parseDate("15-07-2012"), 0.21, 0.1, 0.04, 0.0)
+enum class TipusIVA(
+    val dataIniciVigencia: Date,
+    val preuGeneral: Double,
+    val preuReduit: Double,
+    val preuSuperreduit: Double,
+    val preuExempt: Double
+) {
+    INICIAL(parseDate("01-01-1986"), 0.12, 0.06, 0.0, 0.0), ANY_1992(
+        parseDate("01-01-1992"),
+        0.15,
+        0.06,
+        0.0,
+        0.0
+    ),
+    ANY_1993(parseDate("01-01-1993"), 0.15, 0.06, 0.03, 0.0), ANY_1995(
+        parseDate("01-01-1995"),
+        0.16,
+        0.07,
+        0.04,
+        0.0
+    ),
+    ANY_2010(parseDate("01-01-2010"), 0.18, 0.08, 0.04, 0.0), JULIOL_2012(parseDate("15-07-2012"), 0.21, 0.1, 0.04, 0.0)
 }
 
 /**
@@ -38,7 +55,8 @@ fun parseDate(dateString: String): Date {
  * @return un INT amb el preu amb IVA
  **/
 fun calcularIVA(preu: Double, tipusIVA: TipusIVA): Double {
-    return preu * tipusIVA.preuGeneral
+    val result = preu * tipusIVA.preuGeneral
+    return preu + result
 }
 
 fun main() {
@@ -46,14 +64,27 @@ fun main() {
 
     print("Introdueix el preu sense IVA: ")
     val preuSenseIVA = scanner.nextDouble()
+    println(
+        """
+    Tria el tipus d'IVA: 
+    0- INICIAL
+    1- ANY_1992
+    2- ANY_1993
+    3- ANY_1995
+    4- ANY_2010
+    5- JULIOL_2012
+""".trimIndent()
+    )
 
-    println("Tria el tipus d'IVA (0: INICIAL, 1: ANY_1992, 2: ANY_1993, 3: ANY_1995, 4: ANY_2010, 5: JULIOL_2012): ")
     val opcioTipusIVA = scanner.nextInt()
-    val tipusIVA = TipusIVA.values()[opcioTipusIVA]
-
+    val tipusIVA = TipusIVA.entries[opcioTipusIVA]
     val preuAmbIVA = calcularIVA(preuSenseIVA, tipusIVA)
 
-    println("Preu sense IVA: $preuSenseIVA")
-    println("Tipus d'IVA: $tipusIVA")
-    println("Preu amb IVA: $preuAmbIVA")
+    println(
+        """
+    Preu sense IVA: $preuSenseIVA
+    Tipus d'IVA: $tipusIVA
+    Preu amb IVA: $preuAmbIVA
+    """.trimIndent()
+    )
 }
