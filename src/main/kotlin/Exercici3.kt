580e02935907e1d51dd318a19d1417b36b355f78
niky <nikita.barbosa.7e5@itb.cat>
2023-12-13 21:52:05 +0100

    exercici 3 final

--- src/main/kotlin/Exercici3.kt
+++ src/main/kotlin/Exercici3.kt
@@ -1,42 +1,51 @@
-/*
-3. Escriu una funció anomenada “roman” al que se li passa un número enter
-entre 1 i 3999 i retorni un String amb el seu valor en números romans.
+import java.util.*
+
+/**
+ * Returns a roman number from an arabic number
+ * @param num Int
+ * @return String
+ * @author Nikita Barbosa, Fabián Gómez,
  */
 
-//TODO("mejorar la interfaz")
 data class RomanNumber(val arabic: Int, val roman: String)
 
 fun roman(num: Int): String {
-    println("Introdueix un número entre 1 i 3999")
-    val ROMAN_NUMBER_OPTIONS = listOf(
-    RomanNumber(1, "I"),
-    RomanNumber(4, "IV"),
-    RomanNumber(5, "V"),
-    RomanNumber(9, "IX"),
-    RomanNumber(10, "X"),
-    RomanNumber(40, "XL"),
-    RomanNumber(50, "L"),
-    RomanNumber(90, "XC"),
-    RomanNumber(100, "C"),
-    RomanNumber(400, "CD"),
-    RomanNumber(500, "D"),
-    RomanNumber(900, "CM"),
-    RomanNumber(1000, "M")
-)
-    var remainingNumber = num
-    var resultRoman = ""
-    var availableIndexes = ROMAN_NUMBER_OPTIONS.lastIndex
-    while (remainingNumber > 0) {
-        if (remainingNumber >= ROMAN_NUMBER_OPTIONS[availableIndexes].arabic) {
-            resultRoman += ROMAN_NUMBER_OPTIONS[availableIndexes].roman
-            remainingNumber -= ROMAN_NUMBER_OPTIONS[availableIndexes].arabic
-        } else {
-            availableIndexes--
+    var result = ""
+    if (num < 0) {
+        result = "No es pot convertir el número a romànic perquè és negatiu"
+    } else {
+        val ROMAN_NUMBER_OPTIONS = listOf(
+            RomanNumber(1, "I"),
+            RomanNumber(4, "IV"),
+            RomanNumber(5, "V"),
+            RomanNumber(9, "IX"),
+            RomanNumber(10, "X"),
+            RomanNumber(40, "XL"),
+            RomanNumber(50, "L"),
+            RomanNumber(90, "XC"),
+            RomanNumber(100, "C"),
+            RomanNumber(400, "CD"),
+            RomanNumber(500, "D"),
+            RomanNumber(900, "CM"),
+            RomanNumber(1000, "M")
+        )
+        var remainingNumber = num
+        var resultRoman = ""
+        var availableIndexes = ROMAN_NUMBER_OPTIONS.lastIndex
+        while (remainingNumber > 0) {
+            if (remainingNumber >= ROMAN_NUMBER_OPTIONS[availableIndexes].arabic) {
+                resultRoman += ROMAN_NUMBER_OPTIONS[availableIndexes].roman
+                remainingNumber -= ROMAN_NUMBER_OPTIONS[availableIndexes].arabic
+            } else {
+                availableIndexes--
+            }
         }
+        result = resultRoman
     }
-    return resultRoman
+    return result
 }
-
 fun main() {
-    println(roman(3999))
+    val sc = Scanner(System.`in`)
+    val userNum = checkInt(sc)
+    println(roman(userNum))
 }
