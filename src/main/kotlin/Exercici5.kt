f9a1376781bc02d184d49b01b0ac94bc4c4eb4b7
niky <nikita.barbosa.7e5@itb.cat>
2023-12-13 19:59:27 +0100

    ex6

--- src/main/kotlin/Exercici5.kt
+++ src/main/kotlin/Exercici5.kt
@@ -8,6 +8,6 @@ fun main(){
 
 fun titol(text:String){
     var espais = "                                          "
-    var amplada =kotlin.math.round((80.0 - text.length) / 2).toInt()
+    var amplada = kotlin.math.round((80.0 - text.length) / 2).toInt()
     println(espais.substring(amplada) + text + espais.substring(amplada))
 }
