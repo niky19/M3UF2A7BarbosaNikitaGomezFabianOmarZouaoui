c2cb37aa42f7e46140a781a161a1ad258dbbf441
niky <nikita.barbosa.7e5@itb.cat>
2023-12-13 23:08:14 +0100

    exercici6

--- src/main/kotlin/Exercici6.kt
+++ src/main/kotlin/Exercici6.kt
@@ -1,63 +1,92 @@
-/*
-6. Implementa un programa que faci servir un tipus de dada anomenat “Pila”.
-Una pila és un tipus de dada on es van afegint valors i es van traient en l’ordre
-invers al qual s’han afegit, també conegut com LIFO (Last In First Out). El
-programa ha de presentar el següent menú d’opcions:
-1.- Afegir número (push)
-2.- Treure número (pop)
-3.- Mostrar contingut de la pila
-4.- Sortir
-La pila ha d’admetre com a màxim 10 valors. Si intentem afegir un onzè
-element el sistema no ho ha de permetre i ha d’avisar a l’usuari que la pila es
-troba plena. De la mateixa manera, si intentem treure un número d’una pila que
-es troba buida, també s’ha d’informar a l’usuari que no pot fer aquesta
-operació.
-En treure un número de la pila, s’ha de mostrar per pantalla quin és el número
-que s’ha tret.
-El programa s’ha d’implementar amb un array. No es poden fer servir els tipus
-de dades de les llibreries de Kotlin.
+import java.util.Scanner
+
+/**
+ * Crea, afageix i treu elements d'una pila
+ * @property elements Array<Int>
+ * @author Nikita Barbosa, Omar Zouaoui, Fabián Gomez
  */
+
+//Aquest exercici no té testos perquè interacciona amb l'usuari
+
 fun main() {
-    val myPila = Pila(arrayOf(1,2,3,4,5,6,7,8,9,10))
-    myPila.push(10)
-    myPila.push(20)
-    println(myPila)
+    val sc = Scanner(System.`in`)
+    val myPila = Pila(arrayOf())
+    chooseOption(sc, myPila)
 }
 
 data class Pila(var elements: Array<Int>) {
     fun isFull(): Boolean {
-        var result: Boolean
-        if (elements.size <= 10) {
-            result = true
-        } else {
-            result = false
-        }
-        return result
-    }
+    return elements.size == 10
+}
 
     //Adds a number to the pile
     fun push(numToAdd : Int){
-            if (isFull()) {
-                pop()
-                elements[elements.lastIndex] = numToAdd
-                println(elements)
+            if (!isFull()) {
+                var newElements = Array(elements.size+1){0}
+                for (i in 0..elements.lastIndex){
+                    println(elements[i])
+                    newElements[i] = elements[i]
+                }
+                newElements[newElements.lastIndex] = numToAdd
+                elements = newElements
+            }
+            else {
+                println("La pila està plena. No es pot afegir més elements")
             }
     }
 
-    //Removes last number from the Pile
+    //Removes first number from the Pile
     fun pop() {
-    if (!isFull()) {
-        val newArray = Array(elements.size - 1) { 0 }
-        for (i in 1..<elements.size) {
-            newArray[i - 1] = elements[i]
+        if(elements.size > 0){
+            var newElements = Array(elements.size-1){0}
+            for (i in 0..elements.lastIndex-1){
+                newElements[i] = elements[i]
+            }
+            println("S'ha tret el número ${elements[elements.lastIndex]}")
+            elements = newElements
+        } else {
+            println("La pila està buida")
         }
-        elements = newArray
     }
 }
+
+fun showMenu(){
+    println("""
+        1.- Afegir número (push)
+        2.- Treure número (pop)
+        3.- Mostrar contingut de la pila
+        4.- Sortir
+    """.trimIndent())
 }
-/*
-fun afegirNumero(pila: Pila, num: Int): Pila {val pilaAux = pila.elements
-    pilaAux[pilaAux.lastIndex] = num
-    return Pila(pilaAux)
+
+fun chooseOption(sc : Scanner, pila: Pila){
+    do {
+        showMenu()
+        val option = readOption(sc)
+        when (option) {
+            1 -> {
+                val numToAdd = checkInt(sc)
+                pila.push(numToAdd)
+            }
+            2 -> {
+                pila.pop()
+            }
+            3 -> {
+                println(pila)
+            }
+        }
+    } while (option != 4)
 }
- */
+
+fun readOption(sc: Scanner): Int {
+    var opcio: Int
+    do {
+        opcio = checkInt(sc)
+        if (opcio !in 1..4) {
+            println("Introdueix una opció vàlida")
+        }
+    } while (opcio !in 1..4)
+    return opcio
+}
+
+
